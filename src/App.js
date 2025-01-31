541add9980aa4b80626574c288ad0761ebc61615
unknown <artemijspizhav@gmail.com>
2025-01-31 17:37:53 +0200

    'additional-scroll'

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Security from './Pages/Security'
 import Company from './Pages/Company'
+import ScrollToTop from './Scrool'
 
 function App() {
 	return (
 		<Router>
+			<ScrollToTop />
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/company' element={<Company />} />
